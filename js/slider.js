75cf2daaa567812cc5d2100b06feabf77ee12a12
d3vi1 <d3vi190@rambler.ru>
2014-07-25 16:26:08 +0600

    add more files

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+$(function(){
+	$("#slider").anythingSlider({
+						theme: "metallic",
+						mode: "fade",
+						showMultiple: 1,
+						buildNavigation: false,
+						buildStartStop: false,
+						appendForwardTo: " ",
+						appendBackTo: " ",
+						forwardText: " ",
+						backText: " ",
+						resizeContents: false,
+						enableStartStop: false,
+						autoPlay: true,
+						hashTags: false,
+						delay: 4000
+						});
+});
